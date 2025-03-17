6d78b245313785ed96846ff22c90d2061d18bef9
Xylth <Anthony.cuny@yahoo.fr>
2025-03-17 20:11:12 +0100

    revert

--- Scripts/script.js
+++ Scripts/script.js
@@ -71,7 +71,7 @@ function adjustDesign() {
 
     
     botEl.forEach(el => {
-        el.style.height = (usableHeight * 0.1) + "px"; // 50% de la hauteur du bandeau        
+        el.style.height = (usableHeight * 0.1) + "px"; // 50% de la hauteur du bandea       
     });
     
     
