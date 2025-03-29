39d9c32f868187e9b07cb689d412141e2e12104d
Xylth <Anthony.cuny@yahoo.fr>
2025-03-29 08:07:22 +0100

    retour

--- Scripts/input.js
+++ Scripts/input.js
@@ -3,6 +3,10 @@ let selectedElement=[];
 function initBtn(){
     const addBtn = document.getElementById("addBtn");
     const helpBtn = document.getElementById("help");
+    document.getElementById("back").addEventListener("click", function() {
+        window.location.href = "https://xylth.github.io/Number_match/"; // Remplace par ton URL
+    });
+    
 
     addBtn.addEventListener("click", function() {
         addBtn.setAttribute('status',0);
