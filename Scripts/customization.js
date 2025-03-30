f68a26edb825e0cc103aefc26f637eb9e3350ce6
Xylth <Anthony.cuny@yahoo.fr>
2025-03-30 04:24:14 +0200

    custom

---
+++ Scripts/customization.js
@@ -0,0 +1,17 @@
+function init_custom(){
+    let storedColor;
+    if (localStorage.getItem("backclr") === null) {
+        localStorage.setItem("backclr", "255, 255, 255");
+    }
+    storedColor = localStorage.getItem("backclr");
+    document.documentElement.style.setProperty("--bck-color", `rgb(${storedColor})`);
+    
+    if (localStorage.getItem("borderclr") === null) {
+        localStorage.setItem("borderclr", "000, 000, 000");
+    }
+    if (localStorage.getItem("validnumberclr") === null) {
+        localStorage.setItem("validnumberclr", "000, 000, 000");
+    }
+
+
+}
