068292d4c5262d7dadc093fa5530469780a1b78f
Xylth <Anthony.cuny@yahoo.fr>
2025-03-30 04:40:14 +0200

    Update customization.js

--- Scripts/customization.js
+++ Scripts/customization.js
@@ -1,10 +1,10 @@
 function init_custom(){
     let storedColor;
     if (localStorage.getItem("backclr") === null) {
-        localStorage.setItem("backclr", "255, 255, 000");
+        localStorage.setItem("backclr", "rgb(255, 255, 255)");
     }
     storedColor = localStorage.getItem("backclr");
-    document.documentElement.style.setProperty("--bck-color", `rgb(${storedColor})`);
+    document.documentElement.style.setProperty("--bck-color",storedColor);
 
     if (localStorage.getItem("borderclr") === null) {
         localStorage.setItem("borderclr", "000, 000, 000");
