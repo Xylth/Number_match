56e4c13e1f794f35861fe911846c80573989a97e
Xylth <Anthony.cuny@yahoo.fr>
2025-04-05 22:12:28 +0200

    Update input.js

--- Scripts/input.js
+++ Scripts/input.js
@@ -46,11 +46,11 @@ function initBtn(){
 
     
     aplBtn.addEventListener("click", function() {
-        const bck = document.getElementsByName("bck").values; // récupère le 1er élément avec name="bck"
-        const brd = document.getElementsByName("brd").values; // récupère le 1er élément avec name="brd"
-        const val = document.getElementsByName("valNum").values; // récupère le 1er élément avec name="valNum"
-        const dead = document.getElementsByName("deadNum").values; // récupère le 1er élément avec name="deadNum"
-        const clu = document.getElementsByName("clu").values; // récupère le 1er élément avec name="clu"
+        const bck = document.getElementById("bck").values; // récupère le 1er élément avec name="bck"
+        const brd = document.getElementById("brd").values; // récupère le 1er élément avec name="brd"
+        const val = document.getElementById("valNum").values; // récupère le 1er élément avec name="valNum"
+        const dead = document.getElementById("deadNum").values; // récupère le 1er élément avec name="deadNum"
+        const clu = document.getElementById("clu").values; // récupère le 1er élément avec name="clu"
         localStorage.setItem("backclr", hexToRgb(bck));
         localStorage.setItem("borderclr", hexToRgb(brd));
         localStorage.setItem("validnumberclr",hexToRgb(val));
