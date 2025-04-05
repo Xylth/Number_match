cae6c31cb06718c3edf255cc656b0d22f552289f
Xylth <Anthony.cuny@yahoo.fr>
2025-04-05 21:41:17 +0200

    m

--- Scripts/input.js
+++ Scripts/input.js
@@ -46,12 +46,16 @@ function initBtn(){
 
     
     aplBtn.addEventListener("click", function() {
-        
-        localStorage.setItem("backclr", "rgb(255, 255, 255)");
-        localStorage.setItem("borderclr", "rgb(0, 0, 0)");
-        localStorage.setItem("validnumberclr", "rgb(0, 0, 0)");
-        localStorage.setItem("deadnmbclr", "rgb(164, 164, 164)");
-        localStorage.setItem("clueclr", "rgb(50, 186, 68)");
+        const bck = document.getElementsByName("bck").values; // récupère le 1er élément avec name="bck"
+        const brd = document.getElementsByName("brd").values; // récupère le 1er élément avec name="brd"
+        const val = document.getElementsByName("valNum").values; // récupère le 1er élément avec name="valNum"
+        const dead = document.getElementsByName("brd").values; // récupère le 1er élément avec name="deadNum"
+        const clu = document.getElementsByName("brd").values; // récupère le 1er élément avec name="clu"
+        localStorage.setItem("backclr", hexToRgb(bck));
+        localStorage.setItem("borderclr", hexToRgb(brd));
+        localStorage.setItem("validnumberclr",hexToRgb(val));
+        localStorage.setItem("deadnmbclr", hexToRgb(dead));
+        localStorage.setItem("clueclr",hexToRgb(clu));
         let menuPan= document.getElementById("menu");
         menuPan.style.display="none"
         init_custom();
@@ -79,5 +83,15 @@ function selection_cell(el){
     }
 }
 
+function hexToRgb(hex) {
+    // Supprime le "#" si présent
+    hex = hex.replace("#", "");
+    
+    // Récupère les composantes
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
 
+    return `rgb(${r}, ${g}, ${b})`;
+  }
 
