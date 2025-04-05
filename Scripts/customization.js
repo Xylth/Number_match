63e522b93d1bc62f887c58d32e327484a9fc90ac
Xylth <Anthony.cuny@yahoo.fr>
2025-04-05 04:32:36 +0200

    Revert "a"
    This reverts commit dc8c63698326434b49350f547f5e8348b77501f5.

--- Scripts/customization.js
+++ Scripts/customization.js
@@ -35,48 +35,5 @@ function init_custom(){
     
     storedColor = localStorage.getItem("clueclr");
     document.documentElement.style.setProperty("--clue-color",storedColor);
-
-    
-    if (localStorage.getItem("grdclr") === null) {
-        localStorage.setItem("grdclr", "rgb(127, 127, 127)");
-    }
-    
-    storedColor = localStorage.getItem("grdclr");
-    document.documentElement.style.setProperty("--grd-color",storedColor);
-
-    
-    if (localStorage.getItem("selclr") === null) {
-        localStorage.setItem("selclr", "rgb(42, 67, 151)");
-    }
-    
-    storedColor = localStorage.getItem("selclr");
-    document.documentElement.style.setProperty("--sel-color",storedColor);
-
-    if (localStorage.getItem("failclr") === null) {
-        localStorage.setItem("failclr", "rgb(210, 20, 20)");
-    }
-    
-    storedColor = localStorage.getItem("failclr");
-    document.documentElement.style.setProperty("--fail-color",storedColor);
-
-    
-    if (localStorage.getItem("txtclr") === null) {
-        localStorage.setItem("txtclr", "rgb(0, 0, 0)");
-    }
-    
-    storedColor = localStorage.getItem("txtclr");
-    document.documentElement.style.setProperty("--txt-color",storedColor);
-
-    
-    if (localStorage.getItem("txt2clr") === null) {
-        localStorage.setItem("txt2clr", "rgb(116, 116, 116)");
-    }
-    
-    storedColor = localStorage.getItem("txt2clr");
-    document.documentElement.style.setProperty("--txt2-color",storedColor);
     //    <input type="color" id="colorPicker"></input>
-}
-
-function custom_menu(){
-    
 }
